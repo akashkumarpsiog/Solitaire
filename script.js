6982baca61c37db10cffcf18928addb9082f3e0e
akashkumarpsiog <akash.kumar@psiog.com>
2025-09-22 08:00:03 +0530

    improved the structure

--- script.js
+++ script.js
@@ -1,53 +1,90 @@
+// What things in game?
 
-const restartButton = document.getElementById("restart");
-const scoreDisplay= document.getElementById("score");
 
-restartButton.addEventListener("click", () =>{
-    scoreDisplay.innerHTML= 0;
-})
+/* We have a Card
+A pile - stack of cards (tableau, stock, waste, foundation)
+Tableau - a pile with special rules (descending order, alternating color)
+foundation - pile for completed suits (ascending order)
+stock/waste - pile for draw and discard
+-scores, moves, rules
+*/
 
-const suits = ["hearts", "diamonds", "clubs", "spades"];
-const ranks = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
-// of is for values, in is for index.
-function createDeck() {
-    const deck=[];
-    for (let suit of suits){
-        for(let rank of ranks){
-            deck.push({suit, rank})
-        }
-    }
-    return deck;
-}
-function shuffle(deck){
-    return deck.sort(()=> Math.random() -0.5)
-}
 
-function createCardElement(card, faceUp = false){
-    const cardDiv = document.createElement("div");
-    cardDiv.classList.add("card");
-    if (faceUp){
-        cardDiv.classList.add("face-up");
-        cardDiv.textContent = card.rank + " "+ getSuitSymbol(card.suit);
-    }
-    else {
-        cardDiv.classList.add("face-down");
-    }
-    if (card.suit === "hearts" || card.suit ==="diamonds"){
-        cardDiv.style.color = "red";
-    }
-    else {
-        cardDiv.style.color = "black";
-    }
-    return cardDiv;
-}
+// What are the nouns?
 
-function getSuitSymbol(suit) {
-  switch (suit) {
-    case "hearts":   return "\u2665"; // heart
-    case "diamonds": return "\u2666"; // diamond
-    case "clubs":    return "\u2663"; // clubs
-    case "spades":   return "\u2660"; // spade
-  }
+
+/*
+Card, Pile, tableau, Foundation, Stock, Waste, Move, Score, Rule
+*/
+
+
+//What actions?
+
+
+/*
+Card - flip, show, hide
+Pile - add card, remove card, showTopCard, can add (is it possible?)
+tableau - move card , showTopCard, canAdd
+Foundation - addCard, canAdd
+Stock - drawCard, reset
+Waste - showTopCard, addCard
+Game - start, restart, moveCard, checkWin, updateScore
+*/
+
+/* then we have to define properties- 
+for each class what is the data that we need to store. 
+So we basically create constructors.
+
+
+*/
+
+
+class Game{
+    start(){}
+    restart(){}
+    moveCard(){}
+    checkWin(){}
+    updateScore(){}
 }
 
+class Card{
+    constructor(suit,rank){
+        this.suit = suit;
+        this.rank = rank;
+        this.faceUp=false;
+        this.element = null;
+    }
+    flipCard(){}
+    showCard(){}
+    hideCard(){}
+}
 
+class Pile{
+    addCard(){}
+    removeCard(){}
+    topCard(){}
+    canAdd(){}
+    render() {}
+}
+
+class Tableau extends Pile{
+    moveCard(){}
+    topCard(){}
+    canAdd(){}
+    render() {}
+}
+
+class Foundation extends Pile{
+    addCard(){}
+    canAdd(){}
+    render(){}
+}
+
+class Stock extends Pile{
+    drawCard(){}
+    reset(){}
+}
+class Waste extends Pile{
+    topCard(){}
+    addCard(){}
+}
